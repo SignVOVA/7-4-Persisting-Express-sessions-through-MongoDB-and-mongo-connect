481631c31294f5a68e81129311f47606a9d2c69b
Volodymyr Maksymchuk <volodymyr.maksymchuk@capgemini.com>
2016-07-15 11:25:28 +0100

    server - implemented database use

--- project/server.js
+++ project/server.js
@@ -1,7 +1,10 @@
 var http = require('http'),
 	flights = require('./data'),
+	// We requring db below.
 	db = require('./db'),
-	app = require('./app')(flights);
+	//and now we can pass my db variable below as a parameter
+	//Session will be stored in our mongo db database.
+	app = require('./app')(flights, db);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
