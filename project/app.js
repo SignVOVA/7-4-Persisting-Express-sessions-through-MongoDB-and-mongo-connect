8d793bcf80e0b0ead0dcd922fecd035c675c504f
Volodymyr Maksymchuk <volodymyr.maksymchuk@capgemini.com>
2016-07-15 11:21:17 +0100

    app.js mongoose connect configured within the main app file

--- project/app.js
+++ project/app.js
@@ -3,10 +3,12 @@
  * Module dependencies.
  */
 
-module.exports = function (flights) {
+module.exports = function (flights, db) {
 	var express = require('express');
+	// storing mongo connect in a new variable
+	var MongoStore = require('connect-mongo')(express);
 	var routes = require('./routes')(flights);
-	var path = require('path');	
+	var path = require('path');
 	var app = express();
 
 	// all environments
@@ -15,6 +17,18 @@ module.exports = function (flights) {
 	app.set('view engine', 'jade');
 	app.use(express.favicon());
 	app.use(express.logger('dev'));
+	// Line below is going to read a cookie that the browser sends to the server
+	app.use(express.cookieParser());
+	// Setup the sessions. Takes an object as an argument. This object going to configure the session.
+	app.use(express.session({
+		// Type in a secret. typically we add a randomise string in here. the secret is used to encrypt the session information.
+		secret: 'keyboard cat',
+		// we add a store property. This tells where we want to store the session. Declare a new mongostore instance
+		store: new MongoStore({
+			// we set one property here for mongoose connection. no db connection, but we can pass it in.
+			mongoose_connection: db
+		});
+	}));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(function (req, res, next) {
@@ -36,5 +50,3 @@ module.exports = function (flights) {
 
 	return app;
 }
-
-
